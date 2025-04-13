ccfaa80f9a4339299b8729fb624facd41fc0accf
RavinduTharanga <ravindutharangaperera@gmail.com>
2025-04-13 11:48:39 -0400

    test

--- app/data_loader.py
+++ app/data_loader.py
@@ -1,34 +1,41 @@
 # import pandas as pd
 
-# def load_movies(path="TMDB.csv"):
+# def load_movies(path="https://drive.google.com/drive/u/0/home"):
 #     df = pd.read_csv(path)
 #     df = df[['title', 'overview', 'genres']].dropna()
 #     df['combined'] = df['title'] + " - " + df['overview'] + " Genres: " + df['genres']
 #     return df
 
 
-import pandas as pd
+# from kagglehub import KaggleDatasetAdapter
+# import kagglehub
+# import pandas as pd
 
-# Optional: pip install kagglehub[pandas-datasets]
+# def load_movies():
+#     file_path = kagglehub.load_dataset(
+#         KaggleDatasetAdapter.PANDAS,
+#         "asaniczka/tmdb-movies-dataset-2023-930k-movies",
+#         "TMDB_movie_dataset_v11.csv"
+#     )
+    
+#     df = pd.read_csv(file_path)
+#     df = df[['title', 'overview', 'genres']].dropna()
+#     df['combined'] = df['title'] + " - " + df['overview'] + " Genres: " + df['genres']
+#     return df
+
+
+import pandas as pd
 import kagglehub
 from kagglehub import KaggleDatasetAdapter
 
-# Load the dataset file from KaggleHub
-file_path = kagglehub.load_dataset(
-    KaggleDatasetAdapter.PANDAS,
-    "asaniczka/tmdb-movies-dataset-2023-930k-movies",
-    "TMDB_movie_dataset_v11.csv"  # Make sure this file exists in the dataset
-)
-
-# Define function to process the movie dataset
-def load_movies(file_path):
-    df = pd.read_csv(file_path)
+def load_movies():
+    # Load directly as DataFrame, not file path
+    df = kagglehub.load_dataset(
+        KaggleDatasetAdapter.PANDAS,
+        "asaniczka/tmdb-movies-dataset-2023-930k-movies",
+        "TMDB_movie_dataset_v11.csv"
+    )
     df = df[['title', 'overview', 'genres']].dropna()
     df['combined'] = df['title'] + " - " + df['overview'] + " Genres: " + df['genres']
     return df
-
-# Load and process the dataset
-movies_df = load_movies(file_path)
-print("First 5 records:")
-print(movies_df.head())
 
